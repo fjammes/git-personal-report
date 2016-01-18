36e9d98e5e35400b8d1339d1c8f3613e9f7fdbbb
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2016-01-18 12:29:04 +0100

    Add go report

--- report.go
+++ report.go
@@ -4,19 +4,29 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 )
 
 func run_repo(wg *sync.WaitGroup, repo_url string) {
+	l := strings.Split(repo_url, "/")
+	repo_name := l[len(l)-1]
+	fmt.Printf("INSIDE %s\n", repo_url)
 	const author = "Jammes"
-	cmd_str_list := []string{
-		fmt.Sprintf("git clone --bare %s tmp", repo_url),
-		fmt.Sprintf("cd tmp && git log --pretty=format:\"%s %s\" --author=\"%s\" >> ../stats.txt", repo_url, ": %ad - %an : %s", author)
-	}
+	s := []string{}
+	s = append(s, ("mkdir -p /tmp/gitreport && cd /tmp/gitreport"))
+	s = append(s, fmt.Sprintf("rm -rf %[2]s && git clone --bare %[1]s %[2]s", repo_url, repo_name))
+	s = append(s, fmt.Sprintf("cd %s", repo_name))
+	s = append(s, fmt.Sprintf("git log --pretty=format:\"%[1]s -> %[2]s\" "+
+		"--author=\"%[3]s\" > /tmp/gitreport/stats-%[2]s.txt",
+		"%ad - %an : %s",
+		repo_name,
+		author))
+	cmd_str := strings.Join(s, " && ")
+	fmt.Printf("Running command: %s\n", cmd_str)
 	cmd := exec.Command("sh", "-c", cmd_str)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
-	fmt.Printf("command is: %s\n", cmd_str)
 	err := cmd.Run()
 	if err != nil {
 		panic(err)
@@ -25,13 +35,27 @@ func run_repo(wg *sync.WaitGroup, repo_url string) {
 }
 
 func main() {
-	const repo_url = [...]string{"https://github.com/lsst/qserv", "https://github.com/lsst/qserv_testdata"}
+	repo_urls := [...]string{"https://github.com/lsst/qserv",
+		"https://github.com/lsst/xrootd",
+		"https://github.com/lsst/mariadb",
+		"https://github.com/fjammes/qserv-cluster",
+		"https://github.com/fjammes/vagrant-openstack-example",
+		"https://github.com/fjammes/packer-openstack-example"}
 	const tmp_dir = "tmp"
 	wg := new(sync.WaitGroup)
 
+	wg.Add(len(repo_urls))
 	for _, repo_url := range repo_urls {
-		wg.Add(repo_url)
+		fmt.Printf("Stat for: %s\n", repo_url)
 		go run_repo(wg, repo_url)
-		wg.Wait()
+	}
+	wg.Wait()
+	cmd_str := "cat /tmp/gitreport/stats-* | sort | less"
+	cmd := exec.Command("sh", "-c", cmd_str)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
 	}
 }
